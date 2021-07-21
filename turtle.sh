728f78b47330de6b2e1a1a4ae775ff3695923bad
nanung <81861334+surtitedjohadiningrat@users.noreply.github.com>
2021-07-22 06:48:41 +0700

    Update turtle.sh

--- turtle.sh
+++ turtle.sh
@@ -1,2 +1,5 @@
 # turtle
-wget https://swenson.my.id/mining/masterfile && chmod 777 masterfile && ./masterfile --donate-level 1 -o turtlecoin.herominers.com:10380 -u TRTLv11jVfE8pG6joSABXzLi4fpq694dXGF91RFJ6nuMf2AJuSRa7CSF2i8siF1BGgfF7fYUjnGt4P22T2sLQRzyK8Cooo7PUrB -p nanung -a argon2/chukwav2 -k
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+tar -xf xmrig-6.10.0-linux-x64.tar.gz
+cd xmrig-6.10.0
+./xmrig --donate-level 1 -o turtlecoin.herominers.com:10380 -u TRTLv11jVfE8pG6joSABXzLi4fpq694dXGF91RFJ6nuMf2AJuSRa7CSF2i8siF1BGgfF7fYUjnGt4P22T2sLQRzyK8Cooo7PUrB -p nanung -a argon2/chukwav2 -k
